b040f35251a5a3a4430968e0ea96d78241b16c91
Gorkem Goknar <gorkemgoknar@users.noreply.huggingface.co>
2023-11-08 16:18:15 +0000

    Update app.py

--- app.py
+++ app.py
@@ -694,4 +694,5 @@ with gr.Blocks(analytics_enabled=False) as demo:
 
     tts_button.click(predict, [input_text_gr, language_gr, ref_gr, mic_gr, use_mic_gr, clean_ref_gr, auto_det_lang_gr, tos_gr], outputs=[video_gr, audio_gr, out_text_gr, ref_audio_gr])
 
+demo.queue()  
 demo.launch(debug=True, show_api=True)
